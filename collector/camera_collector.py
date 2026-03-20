03a29a85b6aa8858500914eeba32453494ed0ce3
Sochan001 <250504@softwarica.edu.np>
2026-03-20 11:29:08 +0545

    adding camera feature too collect the image of the user that tries to log in the computer

---
+++ collector/camera_collector.py
@@ -0,0 +1,24 @@
+import sys
+import cv2
+from datetime import datetime
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+PHOTOS_DIR = Path(__file__).resolve().parent.parent / "logs" / "Photos"
+PHOTOS_DIR.mkdir(parents=True, exist_ok=True)
+def collect_camera_events():
+    print("Collecting camera events...\n")
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Could not access the camera.")
+        return
+
+    cap.read()
+    filename = PHOTOS_DIR / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
+    cv2.imwrite(filename, cap.read()[1])
+    print(f"Photo saved: {filename}")
+    # As we know cap.read() returns a tuple (ret, frame), where ret is a boolean indicating if the frame was read successfully and frame is the captured image.
+    # By using cap.read()[1], we directly access the captured image without checking the return value.
+    cap.release()
+
+collect_camera_events()
