e38d7e0762c45197291b682f9ffffcec377cd0bb
Sochan001 <250504@softwarica.edu.np>
2026-03-24 12:26:47 +0545

    adding brute force detection rules

--- analysis/suspicious_detector.py
+++ analysis/suspicious_detector.py
@@ -30,5 +30,21 @@ def detect_unusual_login_times():
             event_time = datetime.strptime(record["timestamp"], "%Y-%m-%d %H:%M:%S")
             if record["event_type"] == "LOGIN_SUCCESS" and event_time.hour <= 5:
                 print(f"⚠  Unusual login time detected: {record['timestamp']} for user {record['user']}")
+
+def detect_brute_force():
+    failed_login=[]
+    with open(AUTH_LOG, "r", encoding="utf-8") as f:
+        for line in f:
+            record = json.loads(line)
+            if record["event_type"] == "LOGIN_FAILED":
+                failed_login.append(record)
+    failed_login.sort(key=lambda r: r["timestamp"])
+    for i in range(len(failed_login) - 1):
+        t1 = datetime.strptime(failed_login[i]["timestamp"], "%Y-%m-%d %H:%M:%S")
+        t2 = datetime.strptime(failed_login[i+1]["timestamp"], "%Y-%m-%d %H:%M:%S")
+        diff = (t2 - t1).total_seconds()
+        if diff <= 300:
+            print("⚠  Potential brute force attack detected!")
 detect_failed_logins()
 detect_unusual_login_times()
+detect_brute_force()
