dd5f53f532d8cbd49cca15033f1156b4fca310e5
Sochan001 <250504@softwarica.edu.np>
2026-03-25 11:53:56 +0545

    adding return instead of print

--- analysis/suspicious_detector.py
+++ analysis/suspicious_detector.py
@@ -5,8 +5,6 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import json
 from config.settings import AUTH_LOG
 def detect_failed_logins():
-    print("Analysing auth events...\n")
-
     failed = []
     with open(AUTH_LOG, "r", encoding="utf-8") as f:
         for line in f:
@@ -14,14 +12,7 @@ def detect_failed_logins():
             if record["event_type"] == "LOGIN_FAILED":
                 failed.append(record)
 
-    if failed:
-        print(f"⚠  Found {len(failed)} failed login attempts:\n")
-        for event in failed:
-            print(f"  {event['timestamp']}  |  EventID: {event['event_id']} | User: {event['user']}")
-    else:
-        print("✓  No failed login attempts found.")
-    if len(failed) >=3:
-            print("\nALERT: SUSPICIOUS!!! More than 3 failed login attempts detected!")
+    return failed
 
 def detect_unusual_login_times():
     with open(AUTH_LOG, "r", encoding="utf-8") as f:
@@ -29,7 +20,9 @@ def detect_unusual_login_times():
             record = json.loads(line)
             event_time = datetime.strptime(record["timestamp"], "%Y-%m-%d %H:%M:%S")
             if record["event_type"] == "LOGIN_SUCCESS" and event_time.hour <= 5:
-                print(f"⚠  Unusual login time detected: {record['timestamp']} for user {record['user']}")
+                return True
+    return False
+
 
 def detect_brute_force():
     failed_login=[]
@@ -44,7 +37,7 @@ def detect_brute_force():
         t2 = datetime.strptime(failed_login[i+1]["timestamp"], "%Y-%m-%d %H:%M:%S")
         diff = (t2 - t1).total_seconds()
         if diff <= 300:
-            print("⚠  Potential brute force attack detected!")
+            return True
 detect_failed_logins()
 detect_unusual_login_times()
 detect_brute_force()
