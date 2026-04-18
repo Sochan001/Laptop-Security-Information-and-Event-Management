9a822569c3b422bb390f9e8d6fdc315556d9e986
Sochan001 <250504@softwarica.edu.np>
2026-04-18 18:39:28 +0545

    fixing sending email bug

--- collector/camera_collector.py
+++ collector/camera_collector.py
@@ -6,17 +6,20 @@ sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from config.settings import PHOTOS_DIR
 
 def capture_photo(reason):
-    print("Collecting camera events...\n")
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Error: Could not access the camera.")
-        return
+        return None                         
 
-    cap.read()
+    cap.read()  
     filename = PHOTOS_DIR / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{reason}.jpg"
-    cv2.imwrite(filename, cap.read()[1])
-    print(f"Photo saved: {filename}")
-    # As we know cap.read() returns a tuple (ret, frame), where ret is a boolean indicating if the frame was read successfully and frame is the captured image.
-    # By using cap.read()[1], we directly access the captured image without checking the return value.
+    ret, frame = cap.read()
     cap.release()
-
+    
+    if not ret:
+        print("Error: Failed to capture frame.")
+        return None
+    
+    cv2.imwrite(str(filename), frame)
+    print(f"Photo saved: {filename}")
+    return filename                         
